5cc6bfe4802d73830c7c200699a76f467c84bfc5
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-03-12 13:00:55 +0800

    ２問目C言語

---
+++ AtCoder Beginners Selection/C/02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b); // aとbの値を入力
+    int product = a * b; // aとbの積を計算
+
+    // 積が偶数か奇数かを判定し、結果を出力
+    if (product % 2 == 0) {
+        printf("Even\n"); // 積が偶数の場合
+    } else {
+        printf("Odd\n"); // 積が奇数の場合
+    }
+
+    return 0;
+}
