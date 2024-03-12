8125ffb383f67ba07b70da4c3629962f02acbfad
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-03-12 11:04:38 +0800

    Atcoder 答え

---
+++ AtCoder Beginners Selection/C/01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    char s[101];
+
+    // 整数 a, b, c を入力
+    scanf("%d%d%d", &a, &b, &c);
+    // 文字列 s を入力
+    scanf("%s", s);
+
+    // a+b+c の計算結果と文字列 s を表示
+    printf("%d %s\n", a + b + c, s);
+
+    return 0;
+}
