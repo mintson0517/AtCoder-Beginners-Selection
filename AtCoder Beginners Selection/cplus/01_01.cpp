c346ec68950759208c3d1945e3e4e8b942e410d9
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-03-16 22:22:47 +0800

    welcome to atcoder のusing namespace stdを使用したバージョン

---
+++ AtCoder Beginners Selection/cplus/01_01.cpp
@@ -0,0 +1,19 @@
+//以下はPracticeA - Welcome to AtCoderのusing namespace std;を使用した場合のコードです
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    string s;
+
+    //整数を入力
+    cin >> a >> b >> c;
+
+    //文字列を入力
+    cin >> s;
+
+    cout << a + b + c << " " << s << endl;
+    return 0;
+}
