ad61c30b69f808f45919e329f4560b8c54f581ef
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-03-16 22:09:02 +0800

    C++始めました

---
+++ AtCoder Beginners Selection/cplus/01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int a, b, c;
+    std::string s;
+
+    //std::cinで整数と文字列を入力
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+    std::cin >> s;
+
+    // 合計と文字列を表示
+    std::cout << (a+b+c) << " " << s << std::endl ;
+
+    return 0;
+}
